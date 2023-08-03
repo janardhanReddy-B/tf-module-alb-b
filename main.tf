05ebb80678be62b84e8cdb2701fd81e5d74065ba
JanardhanReddy <bommireddy246@gmail.com>
2023-08-04 00:56:39 +0100

    terraform

--- main.tf
+++ main.tf
@@ -55,22 +55,22 @@ resource "aws_lb_listener" "public" {
   }
 }
 
-#resource "aws_lb_listener" "private" {
-#  count             = var.name == "private" ? 1 : 0
-#  load_balancer_arn = aws_lb.main.arn
-#  port              = "80"
-#  protocol          = "HTTP"
-#
-#  default_action {
-#    type = "fixed-response"
-#
-#    fixed_response {
-#      content_type = "text/plain"
-#      message_body = "Default Error"
-#      status_code  = "500"
-#    }
-#  }
-#}
+resource "aws_lb_listener" "private" {
+  count             = var.name == "private" ? 1 : 0
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Default Error"
+      status_code  = "500"
+    }
+  }
+}
 
 
 resource "aws_lb_listener" "main" {
